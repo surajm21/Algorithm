e72de13df3b10b336ce1f58450d30796592105d1
suraj <surajmore2143@gmail.com>
2022-06-23 16:04:05 +0530

    Added to merge sort an array feature

---
+++ MergeSort.java
@@ -0,0 +1,50 @@
+package com.bridglabz;
+
+public class MergeSort {
+    int[] array;
+    int[] tempMergeArr;
+    int length;
+
+    public void sort(int inputArr[]) {
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempMergeArr = new int[length];
+        divideArray(0,length-1);
+    }
+
+    public void divideArray(int lowerIndex, int higherIndex) {
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            divideArray(lowerIndex, middle);        //sort left side
+            divideArray(middle + 1, higherIndex);      //sort right side
+            mergeArray(lowerIndex, middle, higherIndex);
+        }
+    }
+
+    public void mergeArray(int lowerIndex, int middle, int higherIndex) {
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergeArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergeArr[i] <= tempMergeArr[j]) {
+                array[k] = tempMergeArr[i];
+                i++;
+            } else {
+                array[k] = tempMergeArr[j];
+            }
+            k++;
+        }
+    }
+    public static void main(String args[]) {
+
+        int[] inputArr = {48, 36, 13, 52, 19, 94, 21};
+        MergeSort ms = new MergeSort();
+        ms.sort(inputArr);
+        for (int i: inputArr){
+            System.out.println(i+" ");
+        }
+    }
+}
